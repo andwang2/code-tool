9dacd672f8c6857bfd0c13b59fa4a093e8447688
andwang2 <andwang2@qq.com>
2019-09-23 21:26:52 +0800

    add

--- elasticsearch/auto-install/install_es.sh
+++ elasticsearch/auto-install/install_es.sh
@@ -2,6 +2,5 @@
 tar -xzvf ./elasticsearch-7.3.2-linux-x86_64.tar.gz
 cp ./jvm.options ./elasticsearch-7.3.2/config
 cp ./elasticsearch.yml ./elasticsearch-7.3.2/config
-#ip=$(ifconfig ens33 | grep "inet " | awk '{ print $2}')
-ip=192.168.7.7
+ip=$(ifconfig eth0 | grep "inet addr:" | awk '{ print $2}'|tr -d "addr:")
 sed -i "s/NETWORKVAL/$ip/g" ./elasticsearch-7.3.2/config/elasticsearch.yml
